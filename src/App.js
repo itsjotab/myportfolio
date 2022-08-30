bda897226ce7e39406a1a27fc14ca9a261842a22
jancy <jotabbusiness@icloud.com>
2022-08-29 21:58:00 -0500

    updated page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import markdown from './markdown.png';
-import randomQuote from './randomQuote.png';
-import robofriends from './robofriends.png';
-import { BsGithub } from 'react-icons/bs';
+import robofriends from './images/robofriends.png';
+import firebase from './images/firebase.png'
+
 
 function App() {
   return (
@@ -24,37 +22,38 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div>
-        <div className='first-box' >
-          <div id='first-post' className="post1">
-            Random Quotes project was build using React components, CSS and Api's in order to retrive the information that is being displayed.
-          </div>
-          <img src={randomQuote} alt='random quote screen-shot' className="first-project" />
-          <div className="project-link">
-            <a href='https://itsjotab.github.io/random-quote-machine-fcc/' className="icon" >
-              <BsGithub />
-            </a>
-          </div>
+
+
+
+      <div id='first-project' className='first-box' >
+        <div className="post1"> Sign-in form using firebase to store user Information, you can create a login or log back in if you have already subscribed.
         </div>
-        <div id='second-project' className='second-box' >
-          <div className="post2">MarkDown Project was build using React components and CSS. </div>
-          <img src={markdown} alt='markdown screen-shot' className="second-project" />
-          <div className="project-link">
-            <a href='https://itsjotab.github.io/markdown-text-fcc/' >
-              <BsGithub />
-            </a>
-          </div>
+        <a href='' >
+          <img src={firebase} alt="Sign-up form" className="second-project" />
+        </a>
+      </div>
+
+
+
+      <div id='second-project' className='second-box' >
+        <div className="post2"> Robofriends project was build using React Components, CSS, multiple librarys using NPM, and Api's.
         </div>
-        <div id='third-project' className='third-box' >
-          <div className="post3"> Robofriends project was build using React Components, CSS, multiple librarys using NPM, and Api's.
-          </div>
+        <a href='https://stock-alerts-ad47f.web.app/' >
+          <img alt="Stocks-portfolio" className="second-project" />
+        </a>
+      </div>
+      
+      
+      <hr />
+      <div id='third-project' className='third-box' >
+        <div className="post3"> Robofriends project uses an API for both users and Robo cards. The search bar allows you to seach for specific users.
+        </div>
+        <a href="https://itsjotab.github.io/robofriends/" >
           <img src={robofriends} alt="robofriends screen-shot" className="third-project" />
-          <div className="project-link">
-            <a href="https://itsjotab.github.io/robofriends/" >
-            <BsGithub />
-            </a>
-          </div>
-        </div>
+        </a>
+      </div>
+      <div>
+        <hr />
       </div>
       <footer className="footer" >Made by Jancy</footer>
     </div>
